19b99411b070f3c538721dd5c37b60a0cddcc67b
keroprogress14 <kerobros2@gmail.com>
2025-10-11 11:24:24 +0700

    11.10.2025 Attack

---
+++ plii/Assets/Scripts/Character/Weapons/AttackComponent.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackComponent : MonoBehaviour
+{
+    [SerializeField] private float _cooldown = 0.35f;
+
+    private IWeapon _weapon;
+    private KeyboardInputReceiver _input;
+    private bool canAttack = true;
+
+    private void Awake()
+    {
+        _weapon = GetComponent<IWeapon>() ?? GetComponentInChildren<IWeapon>();
+        _input = GetComponent<KeyboardInputReceiver>();
+    }
+
+    
+    void Update()
+    {
+        if (!canAttack || _weapon == null || _input == null) return;
+
+        if (_input.AttackPressed)
+        {
+            Debug.Log("нажата кнопка мыши");
+            _weapon.Use();
+            StartCoroutine(CooldownRoutine());
+        }
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(_cooldown);
+        canAttack = true;
+    }
+}
